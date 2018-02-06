3bf4e88d617f05489ceb1180aebe505c4a2fed41
이두영 <leeduyoung20@naver.com>
2018-02-07 00:07:58 +0900

    no message

--- src/providers/http/http.service.ts
+++ src/providers/http/http.service.ts
@@ -1,17 +1,56 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http, URLSearchParams, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
-/*
-  Generated class for the HttpProvider provider.
+import { Url } from './url';
 
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class HttpProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello HttpProvider Provider');
+  headers: Headers;
+  params: URLSearchParams;
+  queryString: string;
+
+  constructor(public http: Http, private url: Url) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json');
+
+    this.params = new URLSearchParams();
+    this.queryString = '';
   }
+
+  appendHeader(key, value) {
+    this.headers.append(key, value);
+  }
+
+  deleteHeader(key) {
+    this.headers.delete(key);
+  }
+
+  // test() {
+  //   let headers = new Headers();
+  //   headers.append('Content-Type', 'application/json');
+  //   return this.http.get(this.url.getUrl().getTest, {headers: headers}).map(res => res.json());
+  // }
+
+  // signIn(user) {
+  //   let body = {
+  //     username: user.id,
+  //     password: user.password,
+  //     grant_type: Environment.grantTypePassword,
+  //     client_id: Environment.clientId,
+  //     client_secret: Environment.clientSecret,
+  //     device_model: this.dataFactory.getDeviceModel().model,
+  //     device_platform: this.dataFactory.getDeviceModel().platform,
+  //     device_uuid: this.dataFactory.getDeviceModel().uuid,
+  //     device_manufacturer: this.dataFactory.getDeviceModel().manufacturer,
+  //     device_version: this.dataFactory.getDeviceModel().version,
+  //     app_version: this.dataFactory.getAppModel().versionNumber
+  //   }
+  //   return this.http.post(this.urlFactory.getUrl().signIn, body, {headers: this.headers}).map(res => res.json()).catch(err => Observable.throw(err.json()));
+  // }
 
 }
--- src/providers/http/url.ts
+++ src/providers/http/url.ts
@@ -1,47 +1,21 @@
-import { appConfig } from "../../config/config";
+import { AppConfig } from "../../config/config";
 
 export class Url {
   host: String;
   apiVersion: String;
 
   constructor() {
-    this.apiVersion = Environment.apiVersion;
-    this.host = Environment.production; //실 서버
-    this.host = Environment.demo; //테스트 서버
-    // this.host = 'http://localhost:3000'; //test
+    this.apiVersion = AppConfig.apiVersion;
+    this.host = AppConfig.production; //실 서버
+    this.host = AppConfig.developer; //테스트 서버
   }
 
   getUrl() {
     let urls = {
-      getTest: this.host + "/v2/test", // test
-      signIn: this.host + "/" + this.apiVersion + "/account/authorization/signin", // 로그인 & 자동 로그인
-      signOut: this.host + "/" + this.apiVersion + "/account/authorization/signout", // 로그아웃
-      signUp: this.host + "/" + this.apiVersion + "/account/signup", // 회원가입
-      deleteAccount: this.host + "/" + this.apiVersion + "/account/revoke", // 회원탈퇴
-      regionList: this.host + "/" + this.apiVersion + "/tags", // 지역태그조회
-      requestAuthCode: this.host + "/" + this.apiVersion + "/account/authorization/confirm-code/send", // 인증번호발송
-      confirmAuthCode: this.host + "/" + this.apiVersion + "/account/authorization/confirm-code/check", // 인증번호확인
-      clubList: this.host + "/" + this.apiVersion + "/clubs", // 시설조회 & 시설목록조회 (page)
-      posterList: this.host + "/" + this.apiVersion + "/posters", // 포스터조회 & 포스터목록조회
-      productList: this.host + "/" + this.apiVersion + "/plans", // 리필상품 목록조회
-      checkIn: this.host + "/" +this.apiVersion + "/checkin/pass", // 입장요청
-      checkInReady: this.host + "/" + this.apiVersion + "/checkin/scan", // 입장 전 화면
-      user: this.host + "/" + this.apiVersion + "/account/information", // 유저정보 조회 & 수정 & setting(push 알림 설정 )
-      shotPack: this.host + "/" + this.apiVersion + "/account/shot-pack", // 보유샷 조회
-      cs: this.host + "/" + this.apiVersion + "/common", // CS 정보 조회
-      premiumClubList: this.host + "/" + this.apiVersion + "/clubs/premium", // 프리미엄 시설 조회
-      shotHistory: this.host + "/" + this.apiVersion + "/account/shot-stack", // 샷 이용현황 조회
-      clubImageList: this.host + "/" + this.apiVersion + "/clubs", // 시설 이미지 조회 (/{version}/clubs/{id}/photos)
-      accessTerms: this.host + "/" + this.apiVersion + "/common/policies", // 이용약관, 개인정보취급방침 조회
-      likeClub: this.host + "/" + this.apiVersion + "/account/like-clubs", // 관심 시설 
-      payment: this.host + "/" + this.apiVersion + "/payment", // 결제 요청 & 결제 완료
-      emailDuplicationCheck: this.host + "/" + this.apiVersion + "/account/check-email",
-      
-      // 결제 (결제정보조회, ...)
-      // club: this.host + "/" + this.apiVersion + "/",
 
-      // shot이용현황조회
-      // club: this.host + "/" + this.apiVersion + "/",
+    //   getTest: this.host + "/v2/test", // test
+    //   signIn: this.host + "/" + this.apiVersion + "/account/authorization/signin", // 로그인 & 자동 로그인
+
     };
     return urls;
   }
